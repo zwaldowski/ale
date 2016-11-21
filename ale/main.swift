a4e08792e1db45182d6f30eae9e6defc24796769
Zachary Waldowski <zach@waldowski.me>
2016-11-21 08:58:05 -0500

    Initial Commit

---
+++ ale/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ale
+//
+//  Created by Zachary Waldowski on 11/21/16.
+//  Copyright © 2016 Zachary Waldowski. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
